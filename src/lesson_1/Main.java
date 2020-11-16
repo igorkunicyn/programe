084f361f17d5bff3f4395412af5e895e32a52b11
igorkunicyn <igor_kunicyn@inbox.ru>
2020-11-16 11:28:22 +0300

    lesson1

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -9,6 +9,9 @@ public class Main {
         out.println(rezCount(2.3456F,6.8908F,89.9876F,7.678F));
         out.println(testSum(8,14));
         whatIsNumber(-5);
+        out.println(whatIsNumber2(-2));
+        helloName("Андрей");
+        whatIsYear(300);
     }
     public static void printVariable(){
         byte b = 121;
@@ -20,7 +23,7 @@ public class Main {
         char c = 'f';
         boolean boo = false;
         String text = "Hello, world";
-        out.println(b+"\\ "+s+"\\ "+i+"\\ "+l+"\\ "+f+"\\ "+d+"\\ "+c+"\\ "+boo+"\\ "+text);
+        out.println(b+" | "+s+" | "+i+" | "+l+" | "+f+" | "+d+" | "+c+" | "+boo+" | "+text);
     }
     public static float rezCount(float a, float b, float c, float d){
        return a*(b+(c/d));
@@ -33,6 +36,19 @@ public class Main {
             out.println("Введенное число - положительное");
         } else {
             out.println("Введенное число - отрицательное");
+        }
+    }
+    public static boolean whatIsNumber2(int a){
+        return a<0;
+    }
+    public static void helloName(String text){
+        out.println("Привет,"+text+"!");
+    }
+    public static void whatIsYear(int a){
+        if ((a%4==0&&!(a%100==0))||a%400==0){
+            out.println("Введенный Вами год - високосный");;
+        }else {
+            out.println("Введенный Вами год - невисокосный");;
         }
     }
 }
