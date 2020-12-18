8cc485fe70d1e0750e7683e1964ebc7bddf23208
igorkunicyn <igor_kunicyn@inbox.ru>
2020-12-18 14:08:20 +0300

    basic

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -1,7 +1,52 @@
 package lesson_1;
 
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
+        RunAndJumpable [] runAndJumpables = {new Human("John"),(RunAndJumpable)new Robot("Boy"),
+                (RunAndJumpable) new Cat("Barsik")};
+//        for (int i = 0; i <runAndJumpables.length ; i++) {
+//                runAndJumpables [i].jump();
+//                runAndJumpables[i].run();
+//        }
 
+        Overcomeable [] overcomeables = {new RunningTrack(), new Wall()};
+//        for (int i = 0; i <runAndJumpables.length ; i++) {
+//            for (int j = 0; j < overcomeables.length ; j++) {
+//                overcomeables[j].notOvercoming(runAndJumpables[i].getName());
+//                overcomeables[j].overcoming(runAndJumpables[i].getName());
+//            }
+//        }
+        int k = (int)(Math.random()*10);//случайным образом определяем последовательность припятствий
+        for (int i = 0; i < k; i++) {
+            Overcomeable overcomeable = overcomeables[0];
+            overcomeables[0] = overcomeables[1];
+            overcomeables[1] = overcomeable;
+        }
+
+        for (int i = 0; i < runAndJumpables.length ;i++ ) {
+            for (int j = 0; j <overcomeables.length  ;j++ ) {
+                if (j==0){
+                    System.out.println();
+                    System.out.println(runAndJumpables[i].getName()+" начал соревнования!");
+                }
+                if (overcomeables[j].getName() == RunningTrack.name) {
+                    if (!overcomeables[j].overOrNotOver(runAndJumpables[i].getName(), runAndJumpables[i].getMaxlength())){
+                        break;
+                    }
+                } else if (overcomeables[j].getName() == Wall.name) {
+                    if (!overcomeables[j].overOrNotOver(runAndJumpables[i].getName(), runAndJumpables[i].getMaxheigth())){
+                        break;
+                    }
+                }
+                if (j==overcomeables.length-1){
+                    System.out.println(runAndJumpables[i].getName()+" закончил соревнования!");
+                    System.out.println();
+                }
+            }
+        }
+        System.out.println();
+        DayOfWeek.getWorkingHouse(DayOfWeek.WEDNESDAY.getNumber());
     }
 }
---
+++ src/lesson_1/MainMain.java
@@ -0,0 +1,7 @@
+package lesson_1;
+
+public class MainMain {
+    public static void main(String[] args) {
+
+    }
+}
