8176bdcc0ae24b3594961bd5a50bb68e5d2355be
igorkunicyn <igor_kunicyn@inbox.ru>
2020-12-27 16:05:17 +0300

    java_fx

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,6 +14,7 @@ public class Main extends Application {
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 500, 500));
         primaryStage.show();
+
     }
 
 
