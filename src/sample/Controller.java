80b9299afef333f091655f7a13f57998b849f57a
igorkunicyn <igor_kunicyn@inbox.ru>
2020-12-30 11:29:10 +0300

    java_fx_2

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,31 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
-
+    @FXML
+    private TextField textfield;
+    @FXML
+    private TextArea textarea;
+    @FXML
+    public void clickBtn(ActionEvent actionEvent) {
+        textarea.appendText(textfield.getText()+"\n");
+        textfield.clear();
+        textfield.requestFocus();
+    }
+    @FXML
+    public void clickEnter(ActionEvent actionEvent) {
+        textarea.appendText(textfield.getText()+"\n");
+        textfield.clear();
+        textfield.requestFocus();
+    }
+    @FXML
+    public void ExitAction(ActionEvent actionEvent) {
+        System.exit(0);
+    }
 }
