aeb68fc70c63738ca2be2c75df1d29f758023ed8
igorkunicyn <igor_kunicyn@inbox.ru>
2021-01-22 16:18:09 +0300

    moven

---
+++ command/src/main/java/Commands/Command.java
@@ -0,0 +1,8 @@
+package Commands;
+
+public class Command {
+    public static final String END = "/end";
+    public static final String AUTH_OK = "/authok";
+    public static final String AUTH = "/auth";
+    public static final String MSG ="/w";
+}
