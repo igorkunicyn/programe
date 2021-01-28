049330ae539db80de3db809254e5883a2a449498
igorkunicyn <igor_kunicyn@inbox.ru>
2021-01-28 16:31:12 +0300

    java_3

--- src/lesson_1/Main.java
+++ src/lesson_1/Main.java
@@ -1,7 +1,23 @@
 package lesson_1;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+        Apple [] apples = new Apple []{new Apple(0.1f),new Apple(0.1f),new Apple(0.1f),
+        new Apple(0.1f),new Apple(0.1f),new Apple(0.1f)};
+        Apple apple = new Apple(0.1f);
+        Box <Apple> appleBox = new Box<Apple>();
+        appleBox.addToBox(apple);
+        appleBox.addToBox(apples);
 
+        Orange [] oranges = new Orange[] {new Orange(1.5f),new Orange(1.5f),new Orange(1.5f),
+        new Orange(1.5f),new Orange(1.5f),new Orange(1.5f)};
+        Orange orange = new Orange(1.5f);
+        Box <Orange> orangeBox = new Box<Orange>();
+        orangeBox.addToBox(oranges);
+        orangeBox.addToBox(orange);
+        System.out.println(appleBox.getBox());
+        System.out.println(orangeBox.getBox());
     }
 }
