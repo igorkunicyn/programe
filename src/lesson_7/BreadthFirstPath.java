07bc47a448d58cfa2240c62b96df0220518714b4
igorkunicyn <igor_kunicyn@inbox.ru>
2021-04-02 09:14:18 +0300

    lesson_7

---
+++ src/lesson_7/BreadthFirstPath.java
@@ -0,0 +1,28 @@
+package lesson_7;
+
+import java.util.LinkedList;
+
+public class BreadthFirstPath extends Path{
+
+    public BreadthFirstPath(Graph g, int source) {
+        super(g, source);
+        bfs(g, source);
+    }
+
+    private void bfs(Graph g, int source) {
+        LinkedList<Integer> queue = new LinkedList<>();
+        queue.addLast(source);
+        marked[source] = true;
+        while (!queue.isEmpty()){
+            int vertex = queue.removeFirst();
+            for (int w : g.getAdjList(vertex)) {
+                if(!marked[w]){
+                    marked[w] = true;
+                    edgeTo[w] = vertex;
+                    queue.addLast(w);
+                }
+            }
+        }
+    }
+
+}
